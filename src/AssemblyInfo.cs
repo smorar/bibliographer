8c7c5bb7914231791999c124e13e47b3e6960dfd
Sameer Morar <smorar@gmail.com>
2009-09-30 05:56:04 +0200

    Update copyright info for assembly Capitilize the assembly title field

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-// Copyright 2005 Sameer Morar <smorar@gmail.com>, Carl Hultquist <chultquist@gmail.com>
+// Copyright 2005-2009 Sameer Morar <smorar@gmail.com>, Carl Hultquist <chultquist@gmail.com>
 // This code is licensed under the GPLv2 license. Please see the COPYING file
 // for more information
 
@@ -11,12 +11,12 @@ using System.Runtime.CompilerServices;
 // change them to the information which is associated with the assembly
 // you compile.
 
-[assembly: AssemblyTitle("bibliographer")]
+[assembly: AssemblyTitle("Bibliographer")]
 [assembly: AssemblyDescription("A bibtex editor for the Gnome Desktop")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright ©2005-2007 Bibliographer developers")]
+[assembly: AssemblyCopyright("Copyright ©2005-2009 Bibliographer developers")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
